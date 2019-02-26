9df8ee8c1d7e85688e2843982312e20f8b5ff5bd
VAGranMax <max.granovski@gmail.com>
2019-02-26 11:18:07 +0300

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Weopen
+    {
+        private string name;
+        public string Name { get; set; }
+        private float kalibr;
+        public float Kalibr { get; set; }
+        private double dalnost;
+        public double Dalnost { get; set; }
+
+        public Weopen()
+        {
+            Name = "Gan";
+            Kalibr = 0;
+            Dalnost = 0.0;
+        }
+        public void Shot()
+        {
+            Console.WriteLine("Введите значения");
+            Name = Console.ReadLine();
+            Kalibr = Convert.ToSingle(Console.ReadLine());
+            Dalnost = Convert.ToDouble(Console.ReadLine());
+        }
+        public void Vvivod()
+        {
+            Console.WriteLine($"Название: {name} Калибр: {kalibr} Дальность {dalnost}");
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+            }
+        }
+    }
+}
+
